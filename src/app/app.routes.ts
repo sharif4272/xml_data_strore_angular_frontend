dfacde7e8926b51be81dc17b156e5c562f30096f
mahmud al hossain <sharif4272@gmail.com>
2025-09-28 18:55:38 +0600

    feat: now this is spa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard';
 import { DashboardHomeComponent } from './dashboard/home/home';
 import { EmployeeListComponent } from './employee-list/employee-list';
-import { EmployeeFormComponent } from './employee-form/employee-form';
-import { EmployeeEditComponent } from './employee-edit/employee-edit';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail';
+// import { EmployeeFormComponent } from './employee-form/employee-form';
+// import { EmployeeEditComponent } from './employee-edit/employee-edit';
+// import { EmployeeDetailComponent } from './employee-detail/employee-detail';
 
 export const routes: Routes = [
   {
@@ -13,25 +13,11 @@ export const routes: Routes = [
     children: [
       { path: '', component: DashboardHomeComponent },
       { path: 'employees', component: EmployeeListComponent },
-      { path: 'add', component: EmployeeFormComponent },
-      { path: 'edit/:id', component: EmployeeEditComponent },
-      { path: 'detail/:id', component: EmployeeDetailComponent }
+      // { path: 'add', component: EmployeeFormComponent },
+      // { path: 'edit/:id', component: EmployeeEditComponent },
+      // { path: 'detail/:id', component: EmployeeDetailComponent }
     ]
   },
   { path: '**', redirectTo: '' }
 ];
 
-
-// import { Routes } from '@angular/router';
-// import { DashboardComponent } from './dashboard/dashboard';
-// import { EmployeeListComponent } from './employee-list/employee-list';
-// import { EmployeeFormComponent } from './employee-form/employee-form';
-// import { EmployeeEditComponent } from './employee-edit/employee-edit';
-// import { EmployeeDetailComponent } from './employee-detail/employee-detail';
-//
-// export const routes: Routes = [
-//   { path: '', component: EmployeeListComponent },
-//   { path: 'add', component: EmployeeFormComponent },
-//   { path: 'edit/:id', component: EmployeeEditComponent },
-//   { path: 'detail/:id', component: EmployeeDetailComponent }
-// ];
